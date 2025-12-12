8b30d76a48ccfa370e6e5e7f80b88c10cb923694
thetechshopgh <thetechshopgh@gmail.com>
2025-12-12 03:08:06 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // ADD .jsx EXTENSIONS HERE
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}', 
   ],
   theme: {
-    extend: {
-      // Define a custom font or colors here if needed later
-    },
+    extend: {},
   },
   plugins: [],
 }
